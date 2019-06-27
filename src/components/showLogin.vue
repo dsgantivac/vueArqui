3c6572dc63ea79ba564b967dda76e245017183c6
David Gantiva <dsgantivac@unal.edu.co>
2019-06-26 19:19:26 -0500

    adding notification variable

--- src/components/showLogin.vue
+++ src/components/showLogin.vue
@@ -63,6 +63,7 @@ export default {
                     email: "`+this.email+`"
                     password: "`+this.password+`"
                     mobil: "false"
+                    messaging: "pruebaaaaa"
                 }){
                     advise
                     token
